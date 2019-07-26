48262e73c1be15308ae643da3e90aa329f44bf6b
Richard <liuyongcheng@rongcloud.cn>
2019-07-26 13:28:27 +0800

    debug

--- com.demo.bootstrap/src/main/java/com/demo/bootstrap/BootstrapServer.java
+++ com.demo.bootstrap/src/main/java/com/demo/bootstrap/BootstrapServer.java
@@ -5,6 +5,8 @@ import com.demo.client.connect.bootstrap.WsServer;
 import com.demo.cluster.manager.bootstrap.ClusterManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by richard on 26/07/2019.
@@ -15,14 +17,14 @@ public class BootstrapServer {
 
     public static void main(String[] args) {
         LOGGER.info("BootstrapServer start.");
-        Thread thread1 = new Thread(()->{
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(() -> {
             WsServer.start();
         });
-        thread1.start();
-        Thread thread2 = new Thread(()->{
+        executorService.submit(() -> {
             ClusterManager.start();
         });
-        thread2.start();
         LOGGER.info("all service started.");
 
     }
